048ac3f5e441b68f6e4044f96d8f3ba96bede23b
Mareike <mareikep@tzi.de>
2015-07-13 16:07:52 +0200

    update logging, scaling test

--- webrob/pages/routes.py
+++ webrob/pages/routes.py
@@ -1,9 +1,12 @@
 from pracWEB.pracinit import pracApp
-import jinja2
 import os
+from os.path import expanduser
+import jinja2
+import logging
+from logging import FileHandler
 
 def register_routes():
-    
+    print 'Registering PRAC routes...'
     from webrob.app_and_db import app
     
     pracApp.app = app
@@ -13,9 +16,26 @@ def register_routes():
         jinja2.FileSystemLoader(['/opt/practools/tools/prac/pracGUI/pracWEB/templates']),
     ])
     pracApp.app.jinja_loader = prac_loader
+    pracApp.app.secret_key = 'so secret!'
     pracApp.app.config['PRAC_STATIC_PATH'] = '/opt/practools/tools/prac/pracGUI/pracWEB/build'
 
-    pracApp.app.secret_key = 'so secret!'
+    # settings for fileuploads and logging
+    home = expanduser("~")
+    pracApp.app.config['ALLOWED_EXTENSIONS'] = set(['mln','db','pracmln'])
+    pracApp.app.config['UPLOAD_FOLDER'] = os.path.join(home, 'pracfiles')
+    pracApp.app.config['LOG_FOLDER'] = os.path.join(pracApp.app.config['UPLOAD_FOLDER'], 'logs')
+    if not os.path.exists(os.path.join(pracApp.app.config['UPLOAD_FOLDER'])):
+       os.mkdir(os.path.join(pracApp.app.config['UPLOAD_FOLDER']))
+
+    if not os.path.exists(os.path.join(pracApp.app.config['LOG_FOLDER'])):
+       os.mkdir(os.path.join(pracApp.app.config['LOG_FOLDER']))
+
+    ulog = logging.getLogger('userstats')
+    ulog.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(message)s")
+    filelogger = FileHandler(os.path.join(pracApp.app.config['LOG_FOLDER'], "userstats.json"))
+    filelogger.setFormatter(formatter)
+    ulog.addHandler(filelogger)
 
 
     from webrob.pages import log
