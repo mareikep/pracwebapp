33751d95b98b5bc63baee650db1f64fd1461b72b
Mareike <mareikep@tzi.de>
2015-10-29 17:18:51 +0100

    addes startup file and updated dockerfile/requirements

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,13 @@ FROM openease/flask
 MAINTAINER Mareike Picklum, mareikep@cs.uni-bremen.de
 USER root
 
-RUN apt-get update
 RUN apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y -q bash build-essential \
-              zlib1g-dev libxml2-dev libxslt1-dev libatlas-base-dev libffi-dev libssl-dev gfortran \
-              texlive-latex-recommended curl git g++ wget libpq-dev \
-              python2.7 python python2.7-dev python-all-dev python-pip python-setuptools python-tk python-support libffi-dev libssl-dev \
+              gfortran libatlas-base-dev libffi-dev libssl-dev curl git \
+              python2.7 python2.7-dev python-pip python-setuptools python-tk python-support python-imaging \
+              wget libpq-dev texlive-latex-recommended \
               openjdk-7-jre default-jre-headless \
-              dvipng libjpeg-dev python-imaging
-
+              dvipng libjpeg-dev libpng-dev pkg-config libfreetype6-dev \
+              zlib1g-dev libxml2-dev libxslt1-dev g++
 
 RUN update-java-alternatives --jre -s java-1.7.0-openjdk-amd64
 ENV JAVA_HOME /usr/lib/jvm/java-7-openjdk-amd64/jre
@@ -19,11 +18,9 @@ ENV DOCKER_LINKS postgres_db:postgres dockerbridge:dockerbridge
 ENV DOCKER_VOLUMES prac_tools prac_data
 
 COPY requirements.txt /tmp/
-RUN echo $JAVA_HOME
-RUN sudo pip install --upgrade pip && sudo pip install -U -r /tmp/requirements.txt
+RUN sudo pip install -U pip && sudo pip install -U -r /tmp/requirements.txt
 
 WORKDIR /opt/webapp
 ADD . /opt/webapp/
 
 ENTRYPOINT /bin/bash /opt/webapp/init.bash
-
---
+++ webrob/startup/init_webapp.py
@@ -0,0 +1,41 @@
+from pracweb.gui.app import pracApp
+import os
+import jinja2
+import logging
+from logging import FileHandler
+
+
+def ulogger(name): return logging.getLogger('userstats')
+
+def init_webapp(app, db):
+    print 'Initializing PRAC webapp...'
+
+    pracApp.app = app
+    # use html templates from prac app
+    prac_loader = jinja2.ChoiceLoader([
+        pracApp.app.jinja_loader,
+        jinja2.FileSystemLoader(['/opt/practools/tools/prac/pracweb/gui/templates']),
+    ])
+    pracApp.app.jinja_loader = prac_loader
+    pracApp.app.config['PRAC_STATIC_PATH'] = '/opt/practools/tools/prac/pracweb/gui/build'
+
+    # settings for fileuploads and logging
+    pracApp.app.config['ALLOWED_EXTENSIONS'] = {'mln', 'db', 'pracmln', 'emln'}
+    pracApp.app.config['UPLOAD_FOLDER'] = '/home/ros/pracfiles'
+    pracApp.app.config['PRAC_ROOT_PATH'] = '/opt/practools/tools/prac'
+    pracApp.app.config['LOG_FOLDER'] = os.path.join('/opt/practools/tools/prac/pracweb/gui/', 'log')
+
+    if not os.path.exists(pracApp.app.config['LOG_FOLDER']):
+        os.mkdir(pracApp.app.config['LOG_FOLDER'])
+
+    # separate logger for user statistics
+    root_logger = logging.getLogger('userstats')
+    handler = FileHandler(os.path.join(pracApp.app.config['LOG_FOLDER'], "userstats.json"))
+    formatter = logging.Formatter("%(message)s,")
+    handler.setFormatter(formatter)
+    root_logger.addHandler(handler)
+
+    print 'Registering PRAC routes...'
+    from pracweb.gui.pages import inference
+    from pracweb.gui.pages import views
+    from pracweb.gui.pages import utils
